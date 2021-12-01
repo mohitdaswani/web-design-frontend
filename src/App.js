de72d180f865038de601a6b07b8971556d1d5a8b
Muralikrishna Shanmuganathan <murali@Muralis-MacBook-Air.local>
2021-12-01 15:33:22 -0500

    Adding frontend skeleton and creating routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ForgotPasswordChange from "./pages/ForgotPasswordChange";
+import HomePage from './pages/HomePage';
+import MoviePlayerPage from "./pages/MoviePlayerPage";
+import AddProductPage from "./pages/AddProductPage";
+import RazorpayPage from './pages/RazorpayPage'
+import AccountPage from "./pages/AccountPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
+import ChangePhoneNoPage from "./pages/ChangePhoneNoPage";
+import MylistPage from "./pages/MylistPage";
+import ChangeEmailPage from "./pages/ChangeEmailPage";
+import SubscriptionPlanPage from "./pages/SubscriptionPlanPage";
+import SearchPage from "./pages/SearchPage";
+import LatestPage from "./pages/LatestPage";
+import AdminLoginPage from "./pages/AdminLoginPage";
+import MoviesPage from "./pages/MoviesPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        
+        <Route exact path="/user/register" component={RegisterPage} />
+        <Route exact path="/user/Login" component={LoginPage} />
+        <Route exact path="/admin/login" component={AdminLoginPage} />
+        <Route exact path="/admin/addMovie" component={AddProductPage} />
+        <Route exact path="/user/forgotPassword" component={ForgotPasswordPage} />
+        <Route exact path="/user/changePassword" component={ChangePasswordPage} />
+        <Route exact path="/user/changePhoneNumber" component={ChangePhoneNoPage} />
+        <Route exact path="/user/changeEmail" component={ChangeEmailPage} />
+        <Route exact path="/user/forgot_password/:token" component={ForgotPasswordChange} />
+        <Route
+          exact
+          path="/user/verify/:token"
+          component={EmailVerificationPage}
+        />
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/movies/language/" component={MoviesPage}/>
+        <Route exact path="/search" component={SearchPage}/>
+        <Route exact path="/mylist" component={MylistPage}/>
+        <Route exact path="/latest" component={LatestPage}/>
+        <Route exact path="/razorpay/:orderId" component={RazorpayPage}/>
+        <Route exact path="/movies/:MovieId" component={MoviePlayerPage}/>
+        <Route exact path="/yourAccount" component={AccountPage}/>
+        <Route exact path="/subscriptionPlans" component={SubscriptionPlanPage}/>
+        <Redirect to="/"/>
+      </Switch>
     </div>
   );
 }
